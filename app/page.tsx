b8e7493a98b41c6620a6a750756ad764cb65f0f0
Mohit Ganna <mohitganna@gmail.com>
2025-05-23 17:48:35 -0400

    Initialized a grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
-export const metadata = {
-  title: "App Router",
-};
+"use client"
+import { useState } from "react";
+import { InputNumber } from 'primereact/inputnumber';
+
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  const [gridLength, setGridLength] = useState<number>(3)
+  const [grid, setGrid] = useState<number[][]>(initGrid())
+
+  function initGrid() {
+    const newGrid: number[][] = []
+    for (let i = 0; i < gridLength; i++) {
+      let arr: number[] = []
+      for (let j = 0; j < gridLength; j++) {
+        arr.push(0)
+      }
+      newGrid.push(arr)
+    }
+    return newGrid
+  }
+
+  return (
+    <table width={'80vw'} >
+      <tbody>
+        { grid.map((row, i) => (
+          <tr key={i}>
+            {
+              row.map((e, j) => (
+                <td key={j}>{e == 0? <input type="number" min={1} max={9} />: e}</td>
+              )) 
+            }
+          </tr>
+          
+        )) }
+      </tbody>
+    </table>
+    
+  )
 }
