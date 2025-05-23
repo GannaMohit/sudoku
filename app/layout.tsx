fdd970a7b77960cf4068855a2bd38e6b8ae8730a
Mohit Ganna <mohitganna@gmail.com>
2025-05-23 18:13:46 -0400

    Initialized primereact

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <PrimeReactProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </PrimeReactProvider>
+    
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 "use client"
 import { useState } from "react";
 import { InputNumber } from 'primereact/inputnumber';
+import { Slider } from "primereact/slider";
 
 
 export default function Page() {
   const [gridLength, setGridLength] = useState<number>(3)
-  const [grid, setGrid] = useState<number[][]>(initGrid())
+  const [grid, setGrid] = useState<number[][]>(initGrid(gridLength))
 
-  function initGrid() {
+  function initGrid(size: number) {
     const newGrid: number[][] = []
-    for (let i = 0; i < gridLength; i++) {
+    for (let i = 0; i < size; i++) {
       let arr: number[] = []
-      for (let j = 0; j < gridLength; j++) {
+      for (let j = 0; j < size; j++) {
         arr.push(0)
       }
       newGrid.push(arr)
@@ -19,21 +20,34 @@ export default function Page() {
     return newGrid
   }
 
+  function handleLengthChange(value: number) {
+    setGridLength(value)
+    setGrid(initGrid(value))
+  }
+
   return (
-    <table width={'80vw'} >
-      <tbody>
-        { grid.map((row, i) => (
-          <tr key={i}>
-            {
-              row.map((e, j) => (
-                <td key={j}>{e == 0? <input type="number" min={1} max={9} />: e}</td>
-              )) 
-            }
-          </tr>
-          
-        )) }
-      </tbody>
-    </table>
+    <div className="align-items-center">
+      <Slider min={2} max={5} value={gridLength} onChange={(event) => handleLengthChange(typeof event.value == 'number' ? event.value: event.value[0])}/>
+
+      <div className="align-items-center justify-content-center w-11 h-11">
+        <table className="w-full h-full border-2 border-collapse">
+          <tbody>
+            { grid.map((row, i) => (
+              <tr key={i} className="w-full h-full border-1 border-collapse">
+                {
+                  row.map((e, j) => (
+                    <td className="border-collapse border-1 align-items-center" key={j}>{e == 0? <InputNumber min={1} max={9} className="border-none"/>: e}</td>
+                  )) 
+                }
+              </tr>
+              
+            )) }
+          </tbody>
+        </table>
+      </div>
+      
+
+    </div>
     
   )
 }
